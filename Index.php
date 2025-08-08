c746605f1724a9cba27fe8108b2162486580fa4b
ActuallyBuns <metrogaming9368@gmail.com>
2025-08-07 20:59:25 -0500

    git log --format=%H%n%aN%n%aE%n%at%n%ct%n%P%n%D%n%B -z -n5 [40ms]

--- Index.php
+++ Index.php
@@ -16,7 +16,7 @@ $user_logged_in = isset($_SESSION['user_id']); // Example check for logged-in us
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My PHP Website</title>
+    <title>balkdielind</title>
     <!-- You can link your CSS files here -->
     <link rel="stylesheet" href="styles.css">
 </head>
@@ -27,12 +27,6 @@ $user_logged_in = isset($_SESSION['user_id']); // Example check for logged-in us
                 <li><a href="index.php">Home</a></li>
                 <li><a href="about.php">About</a></li>
                 <li><a href="contact.php">Contact</a></li>
-                <?php if ($user_logged_in): ?>
-                    <li><a href="profile.php">Profile</a></li>
-                    <li><a href="logout.php">Logout</a></li>
-                <?php else: ?>
-                    <li><a href="login.php">Login</a></li>
-                    <li><a href="register.php">Register</a></li>
                 <?php endif; ?>
             </ul>
         </nav>
